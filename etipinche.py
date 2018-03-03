d7599683f6d66d0527fba58d63336b2a40ccf2b6
chicha_j <julien.chicha@epitech.eu>
2018-03-03 09:44:11 +0100

    meilleure correction

--- etipinche.py
+++ etipinche.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import json
 from modules import Mouli
 
-student_logins = ["chicha_j"]
-student_logins = ["vigand_q"]
+def get_students(path, group_count, group_idx):
+    with open(path, 'r') as f:
+        student_logins = [line.split(';')[0] for line in f.read().strip().split('\n')][1:]
+    print(len(student_logins))
+    group_size = int(len(student_logins) / group_count)
+    begin = group_idx * group_size
+    end = None if group_idx >= group_count - 1 else begin + group_size
+    return student_logins[slice(begin, end)]
+
+student_logins = get_students('students.csv', 4, 0)
 
 with Mouli.load_closing_context("conf/mouli.json", "conf/mailserver.json", verbose=True) as mouli:
     results = mouli.clone_and_test(student_logins, send=True)
-print(results)
+
+with open('/tmp/mouli.out', 'w') as f:
+    json.dump(results, f, indent=4)
